c4a2330099d01059a6e90c0a3420b13ba175ebc5
Mateusz Bogolubow <mateusz@bogolubow.com>
2017-06-21 12:24:26 +0200

    add static data to App

--- jsx/App.jsx
+++ jsx/App.jsx
@@ -3,7 +3,42 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component {
     render () {
-        return <section>App!</section>
+        return <section>
+            <header>
+                <form>
+                    <div><label><input type="text"/></label></div>
+                    <div><label><input type="checkbox"/> Only show cats that like kids</label></div>
+                </form>
+            </header>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Age</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th colSpan="2">male</th>
+                    </tr>
+                    <tr>
+                        <td>Fidel</td>
+                        <td>4</td>
+                    </tr>
+                    <tr>
+                        <td>Fidel</td>
+                        <td>4</td>
+                    </tr>
+                    <tr>
+                        <th colSpan="2">female</th>
+                    </tr>
+                    <tr>
+                        <td>Yude</td>
+                        <td>4</td>
+                    </tr>
+                </tbody>
+            </table>
+        </section>
     }
 }
 
